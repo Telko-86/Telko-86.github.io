544fc96c01e37086a81bc30e989d09587db3d645
Sergi Castillejo Gonzalez <100776649+Telko-86@users.noreply.github.com>
2022-09-17 13:00:47 +0200

    commit change

---
+++ media/all.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script>JAVASCRIPT IS USUALLY PLACED HERE</script>
+<title>Time right now is: </title>
+</head>
+<body>
+<script>
+</script>
+</body>
+</html>
